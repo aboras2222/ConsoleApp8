b0c88f9fd7915fc3b36afd186b430e5f4b346d9d
Eng.Abouras <Eng.Abouras@DESKTOP-GUDPHEL>
2017-09-01 21:29:31 -0700

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        //Faction for (+)...
+        public static int sum1(int x, int y) {
+
+
+
+            return (x + y);
+            }
+        //Faction for (-)...
+        public static float sum2(int x ,int y) {
+
+
+
+
+            return (x - y);
+        }
+        static void Main(string[] args)
+        {
+            int k ,s;
+         
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\t\t\t");
+            
+            s = Convert.ToInt32(Console.ReadLine());
+            float r = sum1(k, s);
+            float d = sum2(s, k);
+            Console.WriteLine(r);
+            Console.Write(d);
+
+          
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
